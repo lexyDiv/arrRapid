9e3908b3a8897bf4e7c906dd8d7985023d137824
lexyDiv <aledab@mail.ru>
2024-06-03 06:27:30 +0300

    pause

--- arrRapid.cpp
+++ arrRapid.cpp
@@ -5,6 +5,8 @@
 #include <set>
 #include <algorithm>
 #include <bits/stdc++.h>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -20,6 +22,9 @@ public:
   {
     this->type = type;
   }
+  void reType(int a){
+       this->type = a;
+  };
   ~Unit()
   {
     // cout << " unit destractor " << endl;
@@ -33,56 +38,92 @@ public:
 // }
 
 void round();
+void round2(int &a);
 
 int main()
 {
 
-  int a = 0;
-  cin >> a;
+  //////////////////////////////////////////
+  vector<Unit *> vec;
+  for (int i = 0; i < 10; i++)
+  {
+    vec.push_back(new Unit(i));
+  }
+  auto filtred = remove_if(vec.begin(), vec.end(), [](Unit *unit)
+                           { return unit->type % 2 == 0; });
+  vec.erase(filtred, vec.end());
+  for (Unit *el : vec)
+  {
+    cout << " type = " << el->type << endl;
+  }
+  ///////////////////////////////////////////////////////////////////////
+
+  Unit* newUnit = new Unit(99);
+  thread rt(&Unit::reType, newUnit, 1000);
+  // thread forUnit([&](){
+  //      newUnit->type = 100;
+  //      cout << " newUnit type = " << newUnit->type << endl;
+  // });
+  cout << " newUnit type = " << newUnit->type << endl;
+
+  auto start = chrono::high_resolution_clock::now();
+  int a = 10;
+
+  thread th(round2, ref(a));
+
   round();
+  // round2(a);
+
+  th.join();
+  rt.join();
+
+  auto finish = chrono::high_resolution_clock::now();
+  chrono::duration<float> resault = finish - start;
+  cout << " timing = " << resault.count() << endl;
+
   while (true)
   {
-    // round();
-    std::this_thread::sleep_for(20ms);
-    // a++;
-    // cout << " a = " << a << endl;
+    this_thread::sleep_for(20ms);
   }
 }
 
 void round()
 {
-
-  int rounds = 0;
+  cout << " stream id = " << this_thread::get_id() << endl;
   int LENGTH = 10;
-
   rapid<Unit *> *arr = new rapid<Unit *>;
-  arr->backForce(LENGTH + 100);
+  arr->backForce(LENGTH);
   for (int i = 0; i < LENGTH; i++)
   {
     arr->push(new Unit(i));
   }
   arr->norm();
-  rapid<Unit *> *arr2 = arr->filter([](Unit *unit, int index)
-                                    { return unit->type % 2 == 0; });
-  // cout << " info = " << arr->arr[1]->type << endl;
-
-  cout << " indexOf " << arr->indexOf(arr2->getItem(2)) << endl;
-
-  // arr->printArr();
-  // cout << "-------------------" << endl;
-  // arr2->printArr();
-  // cout << "-------------------" << endl;
-  // cout << " bool " << (bool)(arr->getItem(2) == arr2->getItem(1)) << endl;
-
   arr->forEach([](Unit *unit)
                {
     delete unit;
     unit = nullptr; });
-
-  delete arr2;
   delete arr;
-  arr2 = nullptr;
-  arr = nullptr;
-
-  // cout << " ALL DELETED !!! " << *a << endl;
+  cout << " all deleted ! " << endl;
 };
+
+void round2(int &a)
+{
+  int LENGTH = 10;
+  rapid<Unit *> *arr2 = new rapid<Unit *>;
+  arr2->backForce(LENGTH);
+  for (int i = 0; i < LENGTH; i++)
+  {
+    arr2->push(new Unit(i));
+    // if(i % 10000000 == 0) {
+    //   this_thread::sleep_for(20ms);
+    //   cout << " sleeping " << endl;
+    // }
+  }
+  arr2->norm();
+  arr2->forEach([](Unit *unit)
+                {
+    delete unit;
+    unit = nullptr; });
+  delete arr2;
+  cout << " all deleted 2 ! " << endl;
+}
