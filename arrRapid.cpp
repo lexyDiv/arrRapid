0832622dad7114d3b96513dbe2a67aaabf2902c5
lexyDiv <aledab@mail.ru>
2024-06-07 09:27:05 +0300

    ver 1

--- arrRapid.cpp
+++ arrRapid.cpp
@@ -1,88 +1,50 @@
 ﻿#include <iostream>
-#include "rapid.h"
-#include <functional>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+
+constexpr int SCREEN_WIDTH {800};
+constexpr int SCREEN_HEIGHT { 600 };
 
 using namespace std;
 
-class Unit
-{
-public:
-  int type = 100;
-  Unit()
-  {
-    // cout << " unit conctructor " << endl;
-  }
-  Unit(int type)
-  {
-    this->type = type;
-  }
-  ~Unit()
-  {
-    // cout << " unit destractor " << endl;
-  }
-};
+int main(int argc, char* args[])
+{   
+ SDL_Window* window{nullptr};
+ SDL_Surface* screenShurface{nullptr};
 
-// template<class T>
-// class box{
-//   public:
-//   arr
-// }
+ if (SDL_Init(SDL_INIT_VIDEO) < 0)
+ {
+ // printf("PIZDA", SDL_GetError());
+ }
+ else
+ {
+  window = SDL_CreateWindow(
+  "SDL TRY",
+   SDL_WINDOWPOS_CENTERED_MASK,
+   SDL_WINDOWPOS_CENTERED_MASK,
+   SCREEN_WIDTH,
+   SCREEN_HEIGHT,
+   SDL_WINDOW_SHOWN
+  );
 
-void round();
-
-int main()
-{
-
-  int a = 0;
-  cin >> a;
-  round();
-  while (true)
+  if (window == nullptr)
   {
-    // round();
-    std::this_thread::sleep_for(20ms);
-    // a++;
-    // cout << " a = " << a << endl;
+  // printf("pizda 2", SDL_GetError());
   }
+  else
+  {
+   screenShurface = SDL_GetWindowSurface(window);
+   SDL_FillRect(
+    screenShurface,
+    nullptr,
+    SDL_MapRGB(screenShurface->format, 255, 255, 255)
+   );
+   SDL_UpdateWindowSurface(window);
+   SDL_Delay(2000);
+  }
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+ }
+
+ return 0;
 }
-
-void round()
-{
-
-  int rounds = 0;
-  int LENGTH = 10;
-
-  rapid<Unit *> *arr = new rapid<Unit *>;
-  arr->backForce(LENGTH + 100);
-  for (int i = 0; i < LENGTH; i++)
-  {
-    arr->push(new Unit(i));
-  }
-  arr->norm();
-  rapid<Unit *> *arr2 = arr->filter([](Unit *unit, int index)
-                                    { return unit->type % 2 == 0; });
-  // cout << " info = " << arr->arr[1]->type << endl;
-
-  cout << " indexOf " << arr->indexOf(arr2->getItem(2)) << endl;
-
-  // arr->printArr();
-  // cout << "-------------------" << endl;
-  // arr2->printArr();
-  // cout << "-------------------" << endl;
-  // cout << " bool " << (bool)(arr->getItem(2) == arr2->getItem(1)) << endl;
-
-  arr->forEach([](Unit *unit)
-               {
-    delete unit;
-    unit = nullptr; });
-
-  delete arr2;
-  delete arr;
-  arr2 = nullptr;
-  arr = nullptr;
-
-  // cout << " ALL DELETED !!! " << *a << endl;
-};
