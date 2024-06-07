6cfe63132374ea2fe2bf6af2fb12efdd7db45535
lexyDiv <aledab@mail.ru>
2024-06-07 11:48:10 +0300

    save

--- arrRapid.cpp
+++ arrRapid.cpp
@@ -1,50 +1,86 @@
 ﻿#include <iostream>
 #include <SDL2/SDL.h>
 
-constexpr int SCREEN_WIDTH {800};
-constexpr int SCREEN_HEIGHT { 600 };
+constexpr int SCREEN_WIDTH{800};
+constexpr int SCREEN_HEIGHT{600};
 
 using namespace std;
 
-int main(int argc, char* args[])
-{   
- SDL_Window* window{nullptr};
- SDL_Surface* screenShurface{nullptr};
+static SDL_Window *gWindow{nullptr};
+static SDL_Surface *gShurface{nullptr};
+static SDL_Surface *loh{nullptr};
 
- if (SDL_Init(SDL_INIT_VIDEO) < 0)
- {
- // printf("PIZDA", SDL_GetError());
- }
- else
- {
-  window = SDL_CreateWindow(
-  "SDL TRY",
-   SDL_WINDOWPOS_CENTERED_MASK,
-   SDL_WINDOWPOS_CENTERED_MASK,
-   SCREEN_WIDTH,
-   SCREEN_HEIGHT,
-   SDL_WINDOW_SHOWN
-  );
+bool Init();
+bool LoadMedia();
+void Close();
 
-  if (window == nullptr)
-  {
-  // printf("pizda 2", SDL_GetError());
-  }
-  else
-  {
-   screenShurface = SDL_GetWindowSurface(window);
-   SDL_FillRect(
-    screenShurface,
-    nullptr,
-    SDL_MapRGB(screenShurface->format, 255, 255, 255)
-   );
-   SDL_UpdateWindowSurface(window);
-   SDL_Delay(2000);
-  }
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+int main(int argc, char *args[])
+{
+    if(!Init() || !LoadMedia())
+    {
+        return -1;
+    }
 
- }
+struct Obj 
+{
+    int x, y;
+    int w, h;
+};
 
- return 0;
+Obj obj{10, 10, 100, 100};
+
+    SDL_BlitSurface(loh, nullptr, gShurface, nullptr);
+    SDL_UpdateWindowSurface(gWindow);
+    SDL_Delay(2000);
+    Close();
+    return 0;
 }
+
+bool Init()
+{
+    bool saccess = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << " bad " << endl;
+        saccess = false;
+    }
+    else
+    {
+        gWindow = SDL_CreateWindow(
+            "image",
+            0,
+            0,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_OPENGL
+        );
+        if(gWindow == nullptr)
+        {
+            saccess = false;
+        }
+        else
+        {
+            gShurface = SDL_GetWindowSurface(gWindow);
+        }
+        return saccess;
+    }
+    return 0;
+}
+
+bool LoadMedia()
+{
+    loh = SDL_LoadBMP("src/loh.bmp");
+    if(loh == nullptr)
+    {
+        printf("pizda");
+        return false;
+    }
+    return true;
+}
+
+void Close()
+{
+    SDL_FreeSurface(loh);
+    SDL_DestroyWindow(gWindow);
+}
+
