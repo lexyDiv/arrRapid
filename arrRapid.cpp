0dcbf51eb47934a46c0c4a9901d09677cfa9d83d
lexyDiv <aledab@mail.ru>
2024-06-18 15:34:46 +0300

    ready to text

--- arrRapid.cpp
+++ arrRapid.cpp
@@ -10,27 +10,10 @@ int main(int argc, char *args[])
 
 	bool quit = false;
 	SDL_Event e;
-	double conor = 0;
-	float x = 200;
-	int y = 300;
-	int speedY = 1;
-	int ax = 0;
+
 
 	Point *arrPoints = new Point[]{{100, 100}, {100, 200}, {200, 200}, {200, 100}};
 
-	// int number = intRand(2, 5000);
-	// Point *arrPoints = new Point[number]{};
-	// for (int i = 0; i < number; i++)
-	// {
-	// 	arrPoints[i] = {intRand(0, 600), intRand(0, 600)};
-	// }
-
-	// for (int i = 0; i < number; i++)
-	// {
-	// 	string res = " i = " + to_string(i) + "( " + "x = " + to_string(arrPoints[i].x) + " y = " + to_string(arrPoints[i].y) + ")";
-	// 	printf(res.c_str());
-	// }
-	// printf(to_string(number).c_str());
 
 	while (!quit)
 	{
@@ -47,11 +30,8 @@ int main(int argc, char *args[])
 		ctx.ClearRect(0, 0, 800, 600);
 
 		ctx.CreateDrawZone(0, 0, 600, 600);
-		ctx.FillRect(0, 0, 600, 600, "yellow");
-		ctx.DrawImage(image, 0, 0, 1536 / 6, 256, x, 100, 300, 300);
+        ctx.FillRect(0, 0, 600, 600, "white");
 
-		ctx.DrawHendleFigure(arrPoints, {170, 130}, 4, conor, "violet");
-		// ctx.DrawLine({200, 300}, {250, 400}, "blue");
 
 		ctx.CreateDrawZone(600, 0, 200, 600);
 		int index = ver * 64;
@@ -75,17 +55,12 @@ int main(int argc, char *args[])
 		{
 			ver = 0;
 		}
+		ctx.FillRect(600, 0, 200, 600, "blue", 100);
 		ctx.DrawImage(miniMap, 0, 0, 64, 64, 600, 30, 200, 200);
 
 		ctx.End();
 
-		x += 2;
-		x == 800 ? x = 0 : x = x;
-		conor += 1;
-		// y += speedY;
-		// if(y <= 0 || y > 600)
-		// {speedY = -speedY;}
-		// std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
 	}
 	ctx.Close();
 
